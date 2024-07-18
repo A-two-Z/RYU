6e16d37013c19133745aeecbf0cb1507e8f7cb0c
이현주 <liveinmars2018@gmail.com>
2024-07-18 09:48:30 +0900

    각 entity에 대응되는 DTO 클래스 생성

--- src/main/java/com/ssafy/mulryuproject/dto/MulProductDTO.java
+++ src/main/java/com/ssafy/mulryuproject/dto/MulProductDTO.java
@@ -1,6 +1,5 @@
 package com.ssafy.mulryuproject.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
---
+++ src/main/java/com/ssafy/mulryuproject/dto/MulSectorDTO.java
@@ -0,0 +1,5 @@
+package com.ssafy.mulryuproject.dto;
+
+public class MulSectorDTO {
+
+}
