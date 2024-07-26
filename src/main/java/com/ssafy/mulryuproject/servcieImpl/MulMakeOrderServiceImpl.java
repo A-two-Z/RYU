ecad396b1a590180d716574f0e42f7f7e1a4f29f
이현주 <liveinmars2018@gmail.com>
2024-07-26 17:57:40 +0900

    feat: MongoDB관련 기능 구현

--- src/main/java/com/ssafy/mulryuproject/servcieImpl/MulMakeOrderServiceImpl.java
+++ src/main/java/com/ssafy/mulryuproject/servcieImpl/MulMakeOrderServiceImpl.java
@@ -47,29 +47,27 @@ public class MulMakeOrderServiceImpl implements MulMakeOrderService {
 		
 		List<MulOrder> orders = orderService.getOrderListByOrderNumberId(orderNum);
 		
+		// 만약 이미 배송이 DELIVER 된 제품이면 자체적으로 차단
+		if(orderNum.getOrderStatus() == MulOrderStatus.DELIVER) {
+			return null;
+		}
+
 		for(MulOrder order : orders) {
 			MulProduct product = productService.getProduct(order).get();
 			List<MulProductSector> sectors = psService.getPSListToProduct(product);
+			MulProductSector sector = getSector(sectors, order.getOrderQuantity());
 			
-			MulProductSector sector = null;
-			
-			for(MulProductSector getSector : sectors) {
-				if(getSector.getQuantity() >= order.getOrderQuantity()) {
-					sector = getSector;
-					break;
-				}
+			if(sector==null) {
 			}
-			
-			if(orderNum.getOrderStatus() == MulOrderStatus.DELIVER)
-				break;
 			
 			MulMakeOrderDetail detail = createToRobot(order, product, sector);
 			
 			// 0724 LHJ productSector 테이블의 quantity를 업데이트 하는 기능의 시점
-//			updateQuantity(sector, order);
+			updateQuantity(sector, order);
 			
 			// 0724 LHJ orderStatus를 Toggle 형식으로 바꿈
-//			orderService.toggleOrderStatus(order);
+			onService.toggleOrderStatus(orderNum);
+			
 			list.add(detail);
 		}
 		
@@ -94,6 +92,21 @@ public class MulMakeOrderServiceImpl implements MulMakeOrderService {
 		psService.updatePSQunatity(quantityUp);
 	}
 	
+	// 0724 LHJ 
+	// 요구하는 Quantity보다 더 많이 갖고 있는 sectors를 가져오는 메소드
+	// 이 클래스 내부에서만 사용된다.
+	private MulProductSector getSector(List<MulProductSector> sectors, int Quantity) {
+		MulProductSector sector = null;
+		
+		for(MulProductSector getSector : sectors) {
+			if(getSector.getQuantity() >= Quantity) {
+				sector = getSector;
+				break;
+			}
+		}
+		
+		return sector;
+	}
 	
 	// 0723 LHJ 
 	// {orderId: 1, product Name: "name", "sector Name": "sector", "orderQuantity" : 20} 하나를 만드는 메소드
