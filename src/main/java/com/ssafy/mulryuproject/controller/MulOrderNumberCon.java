4e941c41df643c4154de4b9c19b6b9cbc2331edf
이현주 <liveinmars2018@gmail.com>
2024-08-05 11:08:28 +0900

    chore: 젠킨스 연동 테스트

--- src/main/java/com/ssafy/mulryuproject/controller/MulOrderNumberCon.java
+++ src/main/java/com/ssafy/mulryuproject/controller/MulOrderNumberCon.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.mulryuproject.entity.MulOrderNumber;
 import com.ssafy.mulryuproject.servcie.MulOrderNumService;
@@ -29,7 +26,7 @@ public class MulOrderNumberCon {
 		return new ResponseEntity<>(list, HttpStatus.ACCEPTED);
 	}
 	@GetMapping("/status_{status}")
-	public ResponseEntity<List<MulOrderNumber>> getOrderNumStatus(@RequestParam String status){
+	public ResponseEntity<List<MulOrderNumber>> getOrderNumStatus(@PathVariable String status){
 		List<MulOrderNumber> list =  service.getOrderNumberList();
 
 		return new ResponseEntity<>(list, HttpStatus.ACCEPTED);
