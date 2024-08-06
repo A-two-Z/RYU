03e6bd2acadbcbe4eb404fd2044f421b7c907f7b
이현주 <liveinmars2018@gmail.com>
2024-08-06 14:53:43 +0900

    feat: sector 정보 가져오는 기능 추가

--- src/main/java/com/ssafy/mulryuproject/controller/MulSectorCon.java
+++ src/main/java/com/ssafy/mulryuproject/controller/MulSectorCon.java
@@ -1,20 +1,36 @@
 package com.ssafy.mulryuproject.controller;
 
+import com.ssafy.mulryuproject.data.MulSectorData;
+import com.ssafy.mulryuproject.entity.MulSector;
+import com.ssafy.mulryuproject.servcie.MulSectorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.mulryuproject.servcie.MulOrderService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/sector")
 public class MulSectorCon {
-	//Create
+	private final MulSectorService sectorService;
+
+    //Create
 	
 	//getList
-	
+	@GetMapping
+    public ResponseEntity<List<MulSector>> getSectorList(){
+        List<MulSector> sectorInfo = sectorService.getSectorList();
+        return new ResponseEntity<>(sectorInfo, HttpStatus.OK);
+    }
 	//getOne
 	
 	//update
