2a5212a7a0beddce35ba3604bd94f066c2d18ac8
이현주 <liveinmars2018@gmail.com>
2024-08-07 18:05:31 +0900

    Test: 연동 테스트

--- src/main/java/com/ssafy/mulryuproject/controller/TEST.java
+++ src/main/java/com/ssafy/mulryuproject/controller/TEST.java
@@ -5,6 +5,7 @@ import com.ssafy.mulryuproject.entity.MulOrderNumber;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,10 @@ import java.util.List;
 @RestController("/status")
 public class TEST {
 
-    @GetMapping
-    public ResponseEntity<List<MulOrder>> orderToQ() { //
+    @PostMapping
+    public ResponseEntity<List<MulOrder>> orderToQ(@RequestBody String getOrder) { //
         System.out.println("OK");
+        System.out.println(getOrder);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
